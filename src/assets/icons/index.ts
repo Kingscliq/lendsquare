814765b2a92d0d9484ee1fee4f962e448b9faf70
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2024-01-04 09:50:50 +0100

    chore: imports of assets

--- src/assets/icons/index.ts
+++ src/assets/icons/index.ts
@@ -1,2 +1,48 @@
 import logo from './logo.svg';
-export { logo };
+import audit from './audit-logs.svg';
+import gurarantor from './guarantors.svg';
+import eye from './eye-view.svg';
+import karma from './karma.svg';
+import carret from './carret.svg';
+
+import loanRequest from './loan-requests.svg';
+import moneySacks from './money-sack.svg';
+import preferences from './preferences.svg';
+import reports from './reports.svg';
+import dashUsers from './dash-users.svg';
+import decisionModels from './decision-models.svg';
+import searchIcon from './search-icon.svg';
+import userWithLoans from './users-with-loans.svg';
+import userWithSavings from './users-with-savings.svg';
+import users from './users.svg';
+import transactions from './transactions.svg';
+import settlements from './settlements.svg';
+import whiteList from './white-list.svg';
+import activateUsers from './activate-user.svg';
+import feesCharges from './fees-charges.svg';
+import feesPricing from './fees-pricing.svg';
+
+export {
+  logo,
+  feesCharges,
+  feesPricing,
+  searchIcon,
+  whiteList,
+  activateUsers,
+  transactions,
+  settlements,
+  users,
+  userWithLoans,
+  userWithSavings,
+  decisionModels,
+  dashUsers,
+  reports,
+  loanRequest,
+  preferences,
+  moneySacks,
+  carret,
+  karma,
+  gurarantor,
+  eye,
+  audit,
+};
