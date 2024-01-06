217f4905fc1f80e318342106c5d3709b7899da2f
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2024-01-06 12:12:11 +0100

    chore: inclusion for form validation for login

--- src/components/elements/Button/index.tsx
+++ src/components/elements/Button/index.tsx
@@ -41,13 +41,12 @@ const Button: React.FC<ButtonProps> = ({
       style={{
         cursor: loading || disabled ? 'not-allowed' : '',
         opacity: loading || disabled ? '.68' : '1',
-        backgroundColor: loading || disabled ? '#363641' : '',
+        backgroundColor: loading || disabled ? 'rgba(57, 205, 204, 0.06)' : '',
+        color: loading || disabled ? 'rgba(12, 33, 28, 0.791)' : 'white',
       }}
     >
       {loading ? (
-        <div className="btn-loader">
-          <BtnLoader />
-        </div>
+        <div className="btn-loader">Loading...</div>
       ) : (
         <div className="btn-body">
           <p className="text-center text-sm md:text-base whitespace-nowrap">
--- src/features/Auth/components/login/form.tsx
+++ src/features/Auth/components/login/form.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
 import styles from './login.module.scss';
 import TextField from '@components/elements/textfield';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import Button from '@components/elements/button';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+
 const Form = () => {
+  const navigate = useNavigate();
+
+  interface FormValues {
+    username: string;
+    password: string;
+  }
+  const intitalValues: FormValues = {
+    username: '',
+    password: '',
+  };
+
+  const loginSchema = yup.object().shape({
+    username: yup.string().required('Username is required'),
+    password: yup.string().required('password is required'),
+  });
+
+  const { handleSubmit, handleChange, values, errors, touched } =
+    useFormik<FormValues>({
+      initialValues: intitalValues,
+      validationSchema: loginSchema,
+      onSubmit: async (values: FormValues) => {
+        console.log(values);
+      },
+    });
   return (
-    <div className={styles.form__container}>
+    <form className={styles.form__container}>
       <div className={styles.content}>
         <div className={styles.headings}>
           <h2>Welcome!</h2>
@@ -13,20 +40,36 @@ const Form = () => {
         </div>
         <div>
           <div className={styles.input__container}>
-            <TextField inputClass={styles.input} />
+            <TextField
+              inputClass={styles.input}
+              value={values.username}
+              placeholder="Enter Username"
+              name="username"
+              error={!!errors?.username && touched?.username}
+              onChange={handleChange}
+              message={errors.username}
+            />
           </div>
           <div className={styles.input__container}>
-            <TextField inputClass={styles.input} />
+            <TextField
+              inputClass={styles.input}
+              value={values.password}
+              placeholder="Enter Password"
+              name="password"
+              error={!!errors?.password && touched?.password}
+              onChange={handleChange}
+              message={errors.password}
+            />
           </div>
           <div className={styles.link}>
             <Link to={'/'}>Forgot Password</Link>
           </div>
         </div>
         <div>
-          <Button label="LOG IN" />
+          <Button label="LOG IN" type="submit" />
         </div>
       </div>
-    </div>
+    </form>
   );
 };
 
