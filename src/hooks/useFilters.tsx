0f944b262c8405a372fb4ccee906a99bcd9da635
Ajaezo Kingsley <ajaezokingsley@gmail.com>
2024-01-06 20:11:03 +0100

    chore: adding filtering to the user data by key value pairs

--- src/hooks/useFilters.tsx
+++ src/hooks/useFilters.tsx
@@ -1,5 +1,4 @@
 import { FilterQuery, UserFilterType } from '@/types/datatable';
-import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const initialState: FilterQuery = {
